0d0545f566fbb741242be4d36ac034312f0a64e7
Luan <luan.silva@ab-inbev.com>
2024-11-19 00:43:12 -0300

    feat:init preferences modal

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import { useDisclosure } from "@nextui-org/react";
+
 import { useFeed } from "../hooks";
 
 import { SettingsIcon } from "@/components/Icons/Icons";
 import { Card } from "@/components/Card/Card";
+import { Filters } from "@/components/Filters/Filters";
 
 export default function Home() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   const { data, isLoading } = useFeed();
 
   if (isLoading) {
@@ -17,9 +22,14 @@ export default function Home() {
 
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <Filters isOpen={isOpen} onOpenChange={onOpenChange} />
+
       <div className="flex items-center">
         <h1 className="mr-10">Set your preferences</h1>
-        <SettingsIcon className="text-default-900 cursor-pointer" />
+        <SettingsIcon
+          className="text-default-900 cursor-pointer"
+          onClick={onOpen}
+        />
       </div>
       {data.map((news) => (
         <Card key={news.title} news={news} />
